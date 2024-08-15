05658a79eac180cf86feae64f9fa0d317e5f7bf1
Abhi Borkar <2022bcs096@sggs.ac.in>
2024-08-15 08:59:57 +0530

    minor changes

--- Frontend/frontend/src/app/Admin/pages/create-event/create-event.component.ts
+++ Frontend/frontend/src/app/Admin/pages/create-event/create-event.component.ts
@@ -1,27 +1,9 @@
-import { AsyncPipe } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  signal,
-} from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { map, Observable, startWith } from 'rxjs';
+import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { CreateBtnComponent } from '../../components/reusable/create-btn/create-btn.component';
 import { CreateEventFormComponent } from '../../components/reusable/create-event-form/create-event-form.component';
+import { CreateSubeventFormComponent } from '../../components/reusable/create-subevent-form/create-subevent-form.component';
 
 @Component({
   selector: 'app-create-event',
@@ -30,52 +12,11 @@ import { CreateEventFormComponent } from '../../components/reusable/create-event
     SidebarComponent,
     CreateBtnComponent,
     CreateEventFormComponent,
+    CreateSubeventFormComponent,
   ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './create-event.component.html',
   styleUrl: './create-event.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CreateEventComponent {
-  // eventName = '';
-  // description = '';
-  // head = '';
-  // coHead = '';
-  options: string[] = ['One', 'Two', 'Three'];
-
-  filteredOptions: Observable<string[]>;
-  eventForm: FormGroup;
-
-  constructor() {
-    this.eventForm = new FormGroup({
-      eventName: new FormControl(''),
-      description: new FormControl(''),
-      head: new FormControl(''),
-      coHead: new FormControl(''),
-      myControl: new FormControl(''),
-    });
-  }
-
-  ngOnInit() {
-    this.filteredOptions = this.eventForm.get('head')!.valueChanges.pipe(
-      startWith(''),
-      map((value) => this._filter(value || ''))
-    );
-  }
-
-  createEvent() {
-    
-  }
-
-  private _filter(value: string): string[] {
-    const filterValue = value.toLowerCase();
-
-    return this.options.filter((option) =>
-      option.toLowerCase().includes(filterValue)
-    );
-  }
-  onSubmit() {
-    console.log(this.eventForm.value);
-  }
-  readonly panelOpenState = signal(false);
-}
+export class CreateEventComponent {}
